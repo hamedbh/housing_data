5bb232cfe2a5c6db4a694bb7b0a2b9a46daf03d3
hamedbh <hamed@bastanhagh.com>
2017-03-28 09:24:42 +0000

    Cleaned up and commented the main R script

--- housing_data_wrangling_2017-03-27.R
+++ housing_data_wrangling_2017-03-27.R
@@ -1,67 +1,35 @@
 # Get housing data for 2017 to explore
+# Import libraries for data wrangling
+
 library(httr)
 library(tidyr)
 library(dplyr)
 library(readr)
 library(reshape2)
 library(purrr)
+
+
 # Use the txt file as it seems to work better without the " characters 
-# in the csv
-#url <- "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-2017.txt"
-#url <- "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-2016.txt"
-#download.file(url, "pp-2017.txt", method = "wget")
+# that are in the csv.
 
-# Read in file from .txt
-#df <- read_delim("pp-2017.txt", 
-                 # delim = ",",
-                 # col_names = FALSE,
-                 # quoted_na = FALSE)
 
-# Tidy up column classes and names
-# df$X5 <- as.factor(df$X5)
-# df$X6 <- as.factor(df$X6)
-# df$X7 <- as.factor(df$X7)
-# 
-# df <- rename(df, tuid = X1, price = X2, date_of_transfer = X3, postcode = X4,
-#              property_type = X5, old_new = X6, duration = X7, paon = X8,
-#              saon = X9, street = X10, locality = X11, town = X12, district = X13,
-#              county = X14, ppd_type = X15, record_status = X16) %>% 
-#     separate(postcode, 
-#              into = c("outcode", "incode"), 
-#              sep = "\\ ",
-#              extra = "merge",
-#              fill = "right") %>% 
-#     select(price, date_of_transfer, 
-#            outcode, 
-#            incode,
-#            property_type, 
-#            everything())
-# 
-# head(df)
-# 
-# by_outcode_type <- df %>% 
-#     group_by(outcode, property_type) %>% 
-#     summarise(n = n(),
-#               avg_price = mean(price), 
-#               sd = sd(price), 
-#               threshold = quantile(price, 0.3))
-# 
-# head(by_outcode_type, n = 200)
 
-years <- seq(2012, 2016)
+# Generate the form for the url and download the files
+
 make_url <- function(year) {
-    paste("http://prod.publicdata.landregistry.gov.uk.", 
-          "s3-website-eu-west-1.amazonaws.com/pp-",
+    paste("http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-",
           year,
           ".txt",
           sep = "")
 }
-urls <- map(years, make_url)
+
+urls <- map(seq(2012, 2016), make_url)
 
 map(urls, function(x) {
     download.file(x, basename(x))
 })
 
+# Generate form for the filenames
 make_filename <- function(year) {
     paste("pp-",
           year,
@@ -69,25 +37,22 @@ make_filename <- function(year) {
           sep = "")
 }
 
-filenames <- map(years, make_filename)
+filenames <- map(seq(2012, 2016), make_filename)
 
-datalist <- lapply(filenames, function(x) read_delim(x, 
-                                                     delim = ",",
-                                                     col_names = FALSE,
-                                                     quoted_na = FALSE))
+# Read the files into memory, then bind the data frames by row and delete the 
+# list of data frames to save memory
+datalist <- map(filenames, 
+                function(x) read_delim(x,
+                                       delim = ",",
+                                       col_names = FALSE,
+                                       quoted_na = FALSE))
 
-full_data <- do.call("rbind", datalist)
+full_data <- bind_rows(datalist)
+
 rm(datalist)
-# df_2012 <- read_delim("pp-2012.txt", 
-#                       delim = ",",
-#                       col_names = FALSE,
-#                       quoted_na = FALSE)
-# 
-# df_2013 <- read_delim("pp-2013.txt", 
-#                       delim = ",",
-#                       col_names = FALSE,
-#                       quoted_na = FALSE)
-# 
+
+# Clean up the data: change certain columns to factors, set names for columns, 
+# and reorder them.
 full_data$X5 <- as.factor(full_data$X5)
 full_data$X6 <- as.factor(full_data$X6)
 full_data$X7 <- as.factor(full_data$X7)
@@ -109,20 +74,24 @@ full_data <- rename(full_data, tuid = X1, price = X2, date_of_transfer = X3,
            property_type, 
            everything())
 
-head(full_data)
-
+# Group the data by outcode and property type, then summarise with a few key 
+# stats
 by_outcode_type <- full_data %>% 
     group_by(outcode, property_type) %>% 
-    summarise(n = n(),
+    summarise(n_i = n(),
               avg_price = mean(price), 
               sd = sd(price), 
               threshold = quantile(price, 0.3))
 
-head(by_outcode_type, n = 200)
+# View top of the tibble and write out an intermediate summary file for Excel
+by_outcode_type
 
-write_excel_csv(by_outcode_type, "summarised_housing_data.csv")
+write_excel_csv(by_outcode_type, "summarised_housing_data1_2017-03-28.csv")
 
-
+# Check how many rows have a reasonable number of examples
 nrow(by_outcode_type %>% 
-         filter(n > 10))
+         filter(n_i > 10))
 
+# Use RStudoio Viewer to browse the tibble
+
+View(by_outcode_type)
